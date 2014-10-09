5c42499a66078c702d304919fc8b77b90487c0ea
ps-soft <admin@programming-source.com>
2014-10-09 19:10:19 -0400

    categorized
    categorized mispelled.
    line #5
    http://www.merriam-webster.com/dictionary/categorize
    
    : to put (someone or something) into a group of similar people or things : to put (people or things) into categories

--- locale/English/news_cats.php
+++ locale/English/news_cats.php
@@ -2,7 +2,7 @@
 $locale['400'] = "News Categories";
 $locale['401'] = "Category:";
 $locale['402'] = "Number of Items:";
-$locale['403'] = "None-Categorised News";
+$locale['403'] = "None-Categorized News";
 $locale['404'] = "There are no news items in this category.";
 $locale['405'] = "More";
 $locale['406'] = "Show All Categories";
